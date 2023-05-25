2756b97b6e05d61d946f392ba043b22be7895b0f
Faisal ahmed <faisal2410@yahoo.com>
2023-05-25 20:07:30 +0600

    initial commit

---
+++ note.js
@@ -0,0 +1,25 @@
+/*
+Important Commands:
+1. php artisan db:show
+2. php artisan make:migration create_posts_table
+3. php artisan migrate
+4. php artisan migrate:status
+5. php artisan migrate --pretend
+6. php artisan migrate:reset
+7. php artisan migrate --force
+8. php artisan migrate:rollback
+9. php artisan migrate:rollback --step=2
+10.php artisan migrate:refresh
+11.php artisan migrate:refresh --step=2
+12.php artisan migrate:fresh
+13.php artisan schema:dump  //not working
+14.php artisan schema:dump --prune // not working
+15.php artisan make:migration add_soft_deletes_in_posts_table --table=posts
+16. composer require doctrine/dbal
+17.php artisan make:migration rename_description_to_content_on_posts_table --table=posts
+18.php artisan make:migration drop_deleted_at_from_posts_table --table=posts
+
+
+
+
+*/
